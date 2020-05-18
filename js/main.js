771b0bc9ba72d9060d9fe5b019e9c0ca8c4383a8
Sanjay Developer <sureshsanjay805@gmail.com>
2020-05-18 12:13:30 +0530

    Placed code at right positions Added comments

---
+++ js/main.js
@@ -0,0 +1,45 @@
+$(document).ready(function(){
+
+    var validation = false;
+    var myTimeoutId = null;
+
+    var config = {
+        mode: "text/html",
+        extraKeys: {"Ctrl-Space": "autocomplete"},
+        lineNumbers: true,
+        keyMap:"sublime",
+        tabSize:4,
+    };
+    // initialize editor
+    var editor = CodeMirror.fromTextArea(document.getElementById('editor'),config);
+    editor.setOption("theme", "material-darker");
+
+	function loadHtml(html) {
+		$('#result').contents().find('html').html(html);
+	}
+
+	loadHtml($('#editor').val());
+
+    editor.on('change',function(cMirror){
+
+        if (myTimeoutId!==null) {
+            clearTimeout(myTimeoutId);
+        }
+        myTimeoutId = setTimeout(function() {
+
+                try{
+
+                    loadHtml(cMirror.getValue());
+
+                }catch(err){
+
+                    console.log('err:'+err);
+
+                }
+                
+
+            }, 2000);
+
+        });
+
+});
